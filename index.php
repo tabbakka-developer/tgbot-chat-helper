6bdc430796d14af39f62484b9576bd86ee314bb8
Popov Vladyslav <vlad.popov@autodoc.eu>
2022-01-07 17:03:13 +0200

    added webhook

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
 
+//dev mode
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
 require_once 'vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
